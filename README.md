194e770447c9083e8422c576abdb9f77df2fd371
lewis machabe <lewismachabe@gmail.com>
2023-10-14 03:58:06 +0300

    removal of bugs

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Dark_mode
+#turns the web page into dark mode and light mode
---
+++ script.js
@@ -0,0 +1,16 @@
+const darkbtn = document.getElementById('darkbtn');
+const body = document.body;
+const isDarkMode = localStorage.getItem('isDarkMode') === 'enabled';
+if (isDarkMode){
+  body.classList.add('dark-mode');
+  darkbtn.checked = true;
+}
+darkbtn.addEventListener('change', () => {
+  if (darkbtn.checked){
+    body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'enabled');
+  } else{
+    body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', 'disabled');
+  }
+});
